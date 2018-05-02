6cf93add166eb2c7d5a169523406705764021f4c
Joanne Lee <vernapii@gmail.com>
2018-05-02 21:46:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class App < Sinatra::Base
       @hero = Superhero.new(params[:superhero][:name], params[:superhero][:motto])
       @member1 = Member.new(params[:superhero][:member][0][:name], params[:superhero][:member][0][:power], params[:superhero][:member][0][:bio])
       @member2 = Member.new(params[:superhero][:member][1][:name], params[:superhero][:member][1][:power], params[:superhero][:member][1][:bio])
+      
       erb :super_hero
     end
 end
