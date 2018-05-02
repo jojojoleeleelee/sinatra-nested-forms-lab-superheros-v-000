d2c7941d4d712970382ff1699977ddcac7bff523
Joanne Lee <vernapii@gmail.com>
2018-05-02 21:46:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       @hero = Superhero.new(params[:superhero][:name], params[:superhero][:motto])
-      @member1 = Member.new()
+      @member1 = Member.new(params[:superhero][:member][0][:name], params[:superhero][:member][0][:power], params[:superhero][:member][0][:bio])
       @member2 = Member.new()
       erb :super_hero
     end
